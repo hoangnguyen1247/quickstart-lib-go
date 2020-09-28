a31e5f12abfab8089459f6d6fef0d43a5a8545c9
Hoàng Ng <hoangnguyen1247@gmail.com>
2020-09-28 16:09:43 +0700

    update package

---
+++ quickstart/cmd/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/hoangnguyen1247/quickstart-lib-go/quickstart"
+
+func main() {
+	quickstart.Hello()
+}
--- quickstart/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/hoangnguyen1247/quickstart-lib-go/hello"
-
-func main() {
-	hello.Hello()
-}
